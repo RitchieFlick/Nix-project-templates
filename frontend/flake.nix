e41bacd758305ee6cf4127b0ae9ea65f1a835dd9
Ritchie Flick <ritchie@flick.dev>
2023-03-25 22:04:13 +0100

    Working on Elixir template

---
+++ frontend/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "An Elixir project";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        # Set the Erlang version
+        erlangVersion = "erlangR25";
+        # Set the Elixir version
+        elixirVersion = "elixir_1_14";
+
+        erlang = pkgs.beam.interpreters.${erlangVersion};
+        elixir = pkgs.beam.packages.${erlangVersion}.${elixirVersion};
+        elixir_ls = pkgs.beam.packages.${erlangVersion}.elixir_ls;
+      in rec {
+        # TODO: Add your Elixir package
+        # packages = flake-utils.lib.flattenTree {
+        # } ;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            erlang
+            elixir
+            elixir_ls
+          ];
+        };
+      }
+    );
+}
