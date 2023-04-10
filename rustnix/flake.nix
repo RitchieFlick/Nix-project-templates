1e94aec5f3369856e7a30b4f4e0a2ac431b1f20d
Ritchie Flick <ritchie@flick.dev>
2023-04-10 20:37:32 +0200

    Trying something else

--- rustnix/flake.nix
+++ rustnix/flake.nix
@@ -1,37 +1,27 @@
 {
-  description = "A devShell example";
-
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-      in
-      with pkgs;
-      {
-        devShells.default = mkShell {
-          buildInputs = [
-            openssl
-            pkg-config
-            exa
-            fd
-            rust-bin.beta.latest.default
-          ];
-
-          shellHook = ''
-            alias ls=exa
-            alias find=fd
-          '';
-        };
-      }
-    );
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+  }: let
+    system = "aarch64-darwin";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [rust-overlay.overlays.default];
+    };
+    toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        toolchain
+      ];
+    };
+  };
 }
+
 
