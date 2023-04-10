67200a52a0bf56bfe202cea82ad491b754b4967d
Ritchie Flick <ritchie@flick.dev>
2023-04-10 20:26:52 +0200

    Changed

--- minimalRust/flake.nix
+++ minimalRust/flake.nix
@@ -16,111 +16,22 @@
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
-        llvmPkgs = pkgs.llvmPackages_13;
 
         # get current working directory
         cwd = builtins.toString ./.;
         rust =
           pkgs.rust-bin.fromRustupToolchainFile "${cwd}/rust-toolchain.toml";
-
-        linuxInputs = with pkgs;
-          lib.optionals stdenv.isLinux [
-            valgrind # used in cli tests, see cli/tests/cli_run.rs
-            vulkan-headers
-            vulkan-loader
-            vulkan-tools
-            vulkan-validation-layers
-            xorg.libX11
-            xorg.libXcursor
-            xorg.libXrandr
-            xorg.libXi
-            xorg.libxcb
-          ];
-
-        darwinInputs = with pkgs;
-          lib.optionals stdenv.isDarwin
-          (with pkgs.darwin.apple_sdk.frameworks; [
-            AppKit
-            CoreFoundation
-            CoreServices
-            CoreVideo
-            Foundation
-            Metal
-            Security
-          ]);
-
-        # For debugging LLVM IR
-        debugir = pkgs.stdenv.mkDerivation {
-          name = "debugir";
-          src = pkgs.fetchFromGitHub {
-            owner = "vaivaswatha";
-            repo = "debugir";
-            rev = "b981e0b74872d9896ba447dd6391dfeb63332b80";
-            sha256 = "Gzey0SF0NZkpiObk5e29nbc41dn4Olv1dx+6YixaZH0=";
-          };
-          buildInputs = with pkgs; [ cmake libxml2 llvmPackages_13.llvm.dev ];
-          buildPhase = ''
-            mkdir build
-            cd build
-            cmake -DLLVM_DIR=${pkgs.llvmPackages_13.llvm.dev} -DCMAKE_BUILD_TYPE=Release ../
-            cmake --build ../
-            cp ../debugir .
-          '';
-          installPhase = ''
-            mkdir -p $out/bin
-            cp debugir $out/bin
-          '';
-        };
-
+          
         sharedInputs = (with pkgs; [
-          # build libraries
-          cmake
-          git
-          python3
-          llvmPkgs.llvm.dev
-          llvmPkgs.clang
-          libxkbcommon
-          pkg-config
-          zig # roc builtins are implemented in zig, see compiler/builtins/bitcode/
-
-          # lib deps
-          libffi
-          libxml2
-          ncurses
-          zlib
-          libiconv
-
-          # faster builds - see https://github.com/roc-lang/roc/blob/main/BUILDING_FROM_SOURCE.md#use-lld-for-the-linker
-          llvmPkgs.lld
-          debugir
           rust
-          cargo-criterion # for benchmarks
-          simple-http-server # to view roc website when trying out edits
-          wasm-pack # for repl_wasm
-          jq
         ]);
       in {
 
         devShell = pkgs.mkShell {
-          buildInputs = sharedInputs ++ darwinInputs ++ linuxInputs
-            ++ (if system == "x86_64-linux" then
-              [ pkgs.nixgl.nixVulkanIntel ]
-            else
-              [ ]);
-
-          LLVM_SYS_130_PREFIX = "${llvmPkgs.llvm.dev}";
-          # nix does not store libs in /usr/lib or /lib
-          NIX_GLIBC_PATH =
-            if pkgs.stdenv.isLinux then "${pkgs.glibc.out}/lib" else "";
-          LD_LIBRARY_PATH = with pkgs;
-            lib.makeLibraryPath
-            ([ pkg-config stdenv.cc.cc.lib libffi ncurses zlib ]
-              ++ linuxInputs);
-          NIXPKGS_ALLOW_UNFREE =
-            1; # to run the editor with NVIDIA's closed source drivers
-        };
+          buildInputs = sharedInputs;
 
         formatter = pkgs.nixpkgs-fmt;
+        };
 
         # You can build this package (the roc CLI) with the `nix build` command.
         packages.default = import ./. { inherit pkgs; };
